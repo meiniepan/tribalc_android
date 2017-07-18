3edff43c0cc8bf2268028aff3a090bf9903d0b19
meiniepan <meiniepan@163.com>
2017-07-18 17:19:11 +0800

    首页改版

---
+++ app/src/main/java/com/gs/buluo/app/view/fragment/HighBuyFragment.java
@@ -0,0 +1,21 @@
+package com.gs.buluo.app.view.fragment;
+
+import android.os.Bundle;
+
+import com.gs.buluo.app.R;
+
+/**
+ * Created by Solang on 2017/7/18.
+ */
+
+public class HighBuyFragment extends BaseFragment {
+    @Override
+    protected int getContentLayout() {
+        return R.layout.fragment_high_buy;
+    }
+
+    @Override
+    protected void bindView(Bundle savedInstanceState) {
+
+    }
+}
