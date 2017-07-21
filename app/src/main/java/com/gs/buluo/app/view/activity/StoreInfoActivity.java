88af8e8f1e1530387fee65a3f67bd28c57f21c2a
meiniepan <meiniepan@163.com>
2017-07-21 18:22:18 +0800

    首页改版

--- app/src/main/java/com/gs/buluo/app/view/activity/StoreInfoActivity.java
+++ app/src/main/java/com/gs/buluo/app/view/activity/StoreInfoActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.gs.buluo.app.Constant;
 import com.gs.buluo.app.R;
@@ -14,9 +15,12 @@ import com.gs.buluo.app.adapter.StoreTagsAdapter;
 import com.gs.buluo.app.bean.StoreInfo;
 import com.gs.buluo.app.network.StoreApis;
 import com.gs.buluo.app.network.TribeRetrofit;
+import com.gs.buluo.app.utils.FrescoImageLoader;
 import com.gs.buluo.common.network.BaseResponse;
 import com.gs.buluo.common.network.BaseSubscriber;
 import com.gs.buluo.common.widget.LoadingDialog;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
 
 import java.util.ArrayList;
 
@@ -31,13 +35,17 @@ import rx.schedulers.Schedulers;
 public class StoreInfoActivity extends BaseActivity implements View.OnClickListener {
     @Bind(R.id.recycle_store_tag)
     RecyclerView mRecyclerView;
+    @Bind(R.id.store_info_head)
+    Banner mBanner;
     @Bind(R.id.tv_store_name)
     TextView tvName;
     @Bind(R.id.tv_store_category)
     TextView tvCategory;
     @Bind(R.id.tv_store_mark_place)
-    TextView tvPlace;@Bind(R.id.tv_store_desc)
-    TextView tvDesc;@Bind(R.id.buy)
+    TextView tvPlace;
+    @Bind(R.id.tv_store_desc)
+    TextView tvDesc;
+    @Bind(R.id.buy)
     Button buy;
     private String storeId;
     private ArrayList dataTags;
@@ -50,6 +58,10 @@ public class StoreInfoActivity extends BaseActivity implements View.OnClickListe
     @Override
     protected void bindView(Bundle savedInstanceState) {
         storeId = getIntent().getStringExtra(Constant.STORE_ID);
+        mBanner.setImageLoader(new FrescoImageLoader(false));
+        mBanner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR);
+//        getBannerData();
+        buy.setOnClickListener(this);
         getData();
         mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(8, StaggeredGridLayoutManager.VERTICAL));
         StoreTagsAdapter adapter = new StoreTagsAdapter(getCtx(), dataTags);
@@ -81,9 +93,9 @@ public class StoreInfoActivity extends BaseActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.buy:
-
+                Toast.makeText(this, "haha", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
