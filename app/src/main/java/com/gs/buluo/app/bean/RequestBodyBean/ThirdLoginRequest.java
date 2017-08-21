b92a8a696d1d8b36b0778af445e1c7e869b414e1
侯晶南 <hjn910@163.com>
2017-08-21 18:47:22 +0800

    三方 登录  部分

---
+++ app/src/main/java/com/gs/buluo/app/bean/RequestBodyBean/ThirdLoginRequest.java
@@ -0,0 +1,10 @@
+package com.gs.buluo.app.bean.RequestBodyBean;
+
+/**
+ * Created by hjn on 2017/8/21.
+ */
+
+public class ThirdLoginRequest {
+    public String code;
+    public String memberType;
+}
--- app/src/main/java/com/gs/buluo/app/wxapi/WXEntryActivity.java
+++ app/src/main/java/com/gs/buluo/app/wxapi/WXEntryActivity.java
@@ -6,6 +6,15 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.gs.buluo.app.Constant;
+import com.gs.buluo.app.R;
+import com.gs.buluo.app.bean.RequestBodyBean.ThirdLoginRequest;
+import com.gs.buluo.app.bean.ResponseBody.UserBeanEntity;
+import com.gs.buluo.app.network.MainApis;
+import com.gs.buluo.app.network.TribeRetrofit;
+import com.gs.buluo.app.utils.ToastUtils;
+import com.gs.buluo.common.network.ApiException;
+import com.gs.buluo.common.network.BaseResponse;
+import com.gs.buluo.common.network.BaseSubscriber;
 import com.gs.buluo.common.widget.LoadingDialog;
 import com.tencent.mm.opensdk.constants.ConstantsAPI;
 import com.tencent.mm.opensdk.modelbase.BaseReq;
@@ -13,6 +22,9 @@ import com.tencent.mm.opensdk.modelbase.BaseResp;
 import com.tencent.mm.opensdk.openapi.IWXAPI;
 import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
 import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by hjn on 2016/12/29.
@@ -50,8 +62,30 @@ public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
     @Override
     public void onResp(BaseResp baseResp) {
         Log.e(TAG, "onReq: "+ baseResp.toString());
-//        EventBus.getDefault().post(new TopupEvent());
-//        LoadingDialog.getInstance().dismissDialog();WXPayEntryActivity
-//        finish();
+        ThirdLoginRequest request = new ThirdLoginRequest();
+        request.code = baseResp.transaction;
+        TribeRetrofit.getInstance().createApi(MainApis.class).doThirdLogin(request)
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new BaseSubscriber<BaseResponse<UserBeanEntity>>() {
+            @Override
+            public void onNext(BaseResponse<UserBeanEntity> response) {
+                super.onNext(response);
+            }
+
+            @Override
+            public void onFail(ApiException e) {
+                if (e.getCode()==404){
+                    bindPhone();
+                }else {
+                    ToastUtils.ToastMessage(WXEntryActivity.this, R.string.connect_fail);
+                }
+            }
+        });
+    }
+
+    private void bindPhone() {
+
+
     }
 }
