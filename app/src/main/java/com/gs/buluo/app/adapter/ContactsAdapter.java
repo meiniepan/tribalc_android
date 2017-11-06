eeb99fd800b3b763539aab8822bce16882fe025d
王博宁 <wangboning@buluo-gs.com>
2017-11-06 18:12:03 +0800

    fix会议室预定 checkbox复用

--- app/src/main/java/com/gs/buluo/app/adapter/ContactsAdapter.java
+++ app/src/main/java/com/gs/buluo/app/adapter/ContactsAdapter.java
@@ -1,11 +1,11 @@
 package com.gs.buluo.app.adapter;
 
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 
-import com.gs.buluo.app.bean.ContactsPersonEntity;
 import com.gs.buluo.app.R;
+import com.gs.buluo.app.bean.ContactsPersonEntity;
 import com.gs.buluo.app.view.widget.recyclerHelper.BaseHolder;
 import com.gs.buluo.app.view.widget.recyclerHelper.BaseQuickAdapter;
 
@@ -25,13 +25,18 @@ public class ContactsAdapter extends BaseQuickAdapter<ContactsPersonEntity, Base
         helper.setText(R.id.tv_name, item.name).setText(R.id.tv_number, item.phone);
         CheckBox checkBox = helper.getView(R.id.cb_check);
         if (checkBox != null) {
-            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            checkBox.setChecked(item.checked);
+            checkBox.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    item.checked = isChecked;
+                public void onClick(View v) {
+                    item.checked = !item.checked;
                 }
             });
         }
     }
 
+    @Override
+    public void onBindViewHolder(BaseHolder holder, int positions) {
+        super.onBindViewHolder(holder, positions);
+    }
 }
