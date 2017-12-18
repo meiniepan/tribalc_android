d5bcb6ea71fa24ca68b3b5560ca7e51b01189301
侯晶南 <houjingnan@buluo-gs.com>
2017-12-18 19:16:11 +0800

    搜索

--- app/src/main/java/com/gs/buluo/app/view/activity/MainSearchActivity.java
+++ app/src/main/java/com/gs/buluo/app/view/activity/MainSearchActivity.java
@@ -1,17 +1,40 @@
 package com.gs.buluo.app.view.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.gs.buluo.app.R;
+
+import butterknife.BindView;
 
 /**
  * Created by Solang on 2017/7/17.
  */
 
-public class MainSearchActivity extends BaseActivity{
+public class MainSearchActivity extends BaseActivity {
+    @BindView(R.id.search_edit)
+    EditText etSearch;
+    @BindView(R.id.search_content)
+    TextView tvContent;
+    @BindView(R.id.search_empty)
+    View emptyView;
+
     @Override
     protected void bindView(Bundle savedInstanceState) {
-
+        etSearch.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode==66){
+                    emptyView.setVisibility(View.VISIBLE);
+                    tvContent.setText(etSearch.getText().toString().trim());
+                }
+                return false;
+            }
+        });
     }
 
     @Override
